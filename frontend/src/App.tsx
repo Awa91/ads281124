caf95959932bbdc176f36cb46023ec4d1e89f6a6
Innocent <obasiia73@gmail.com>
2024-11-28 15:32:18 +0100

    comment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,120 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+interface Player {
+  id: number;
+  name: string;
+  age: number;
+  position: string;
+}
+
+interface Photo {
+  id: number;
+  url: string;
+  title: string;
+  albumId: number;
+  thumbnailUrl: string;
+}
+
+interface Member {
+  name: string;
+}
+
+interface SquadResponse {
+  squadName: string;
+  homeTown: string;
+  members: Member[];
+  demos: Member[];
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [players, setPlayers] = useState<Player[]>([]);
+  console.log(players);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [squad, setSquad] = useState<string>("");
+  const [homeTown, setHomeTown] = useState<string>("");
+  const [members, setMembers] = useState<Member[]>([]);
+  const [demo, setDemo] = useState<Member[]>([]);
+
+  console.log(photos);
+  console.log(squad);
+  console.log(members);
+  console.log(demo);
+
+  useEffect(() => {
+    axios
+      .get("https://awa91.github.io/abs0324/players.json")
+      .then((response) => {
+        setPlayers(response.data.players);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://awa91.github.io/ads281124/data.json")
+      .then((response) => {
+        setPhotos(response.data.photos);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get<SquadResponse>("https://awa91.github.io/abs0324/superheroes.json")
+      .then((response) => {
+        setSquad(response.data.squadName);
+        setHomeTown(response.data.homeTown);
+        setMembers(response.data.members);
+        setDemo(response.data.demos);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   return (
     <>
+      <div className="mx-auto">
+        <h1>Players</h1>
+        {players.map((player) => (
+          <div key={player.id}>
+            <h2>{player.name}</h2>
+            <p>{player.age}</p>
+            <p>{player.position}</p>
+          </div>
+        ))}
+      </div>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Photo section</h1>
+        {photos.map((photo) => (
+          <div key={photo.id} className="relative">
+            <img className="h-3 w-3" src={photo.url} alt="" />
+            <p>{photo.title}</p>
+            <p>{photo.albumId}</p>
+            <p>{photo.thumbnailUrl}</p>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <p>{squad}</p>
+      <p>{homeTown}</p>
+      <div>
+        <h1>Demo section</h1>
+        {demo.map((demo) => (
+          <div key={demo.name}>
+            <p>{demo.name}</p>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src="/comment.png" className="w-10 h-10"/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
